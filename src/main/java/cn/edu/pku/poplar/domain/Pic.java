b924b2bbb8052d00178aac6635ba0102965bfc5d
Weiliang Shuai <bitlanghen@gmail.com>
2012-05-31 11:14:05 +0800

    Revert "Revert "Revert "Revert "添加基因图展示功能，以及杜老师的一些改进意见""""
    This reverts commit 3fd10744c8ed8ec0084a776a391ae63cdaa074c9.

--- src/main/java/cn/edu/pku/poplar/domain/Pic.java
+++ src/main/java/cn/edu/pku/poplar/domain/Pic.java
@@ -4,6 +4,9 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord(finders = { "findPicsByReferNameLike", "findPicsByForwardPrimerLike", "findPicsByReversePrimerLike" })
@@ -26,4 +29,27 @@ public class Pic {
     private float na;
 
     private String alleleSizeRange;
+
+    private int isTested;
+
+    public static TypedQuery<Pic> findPicsByTop3(String reversePrimer, String forwardPrimer, String referName) {
+        StringBuilder hql = new StringBuilder("SELECT o FROM Pic AS o");
+        StringBuilder andClause = new StringBuilder();
+        andClause.append(HqlUtils.getAndClauseHql("reversePrimer", reversePrimer));
+        andClause.append(HqlUtils.getAndClauseHql("forwardPrimer", forwardPrimer));
+        andClause.append(HqlUtils.getAndClauseHql("referName", referName));
+        HqlUtils.updateFirstAndToWhere(andClause);
+        hql.append(andClause.toString());
+        EntityManager em = Pic.entityManager();
+        TypedQuery<Pic> q = em.createQuery(hql.toString(), Pic.class);
+        return q;
+    }
+    
+    public String getTests() {
+        if(this.isTested == 1) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
 }
