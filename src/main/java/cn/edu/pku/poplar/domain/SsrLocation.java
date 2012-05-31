6901061fd772f059ff758bfbf89a463784a683e1
Weiliang Shuai <bitlanghen@gmail.com>
2012-05-31 11:13:57 +0800

    Revert "添加基因图展示功能，以及杜老师的一些改进意见"
    This reverts commit 027da27ba3e7188516af96b6c34b29d706fef59b.

--- src/main/java/cn/edu/pku/poplar/domain/SsrLocation.java
+++ src/main/java/cn/edu/pku/poplar/domain/SsrLocation.java
@@ -4,9 +4,6 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord(finders = { "findSsrLocationsByUniGeneIdLike" })
@@ -31,37 +28,4 @@ public class SsrLocation {
     private String seqOfScaffold;
 
     private Float identify;
-
-    public static TypedQuery<SsrLocation> findSsrLocationsByBean(SsrLocation ssrLocation) {
-        if(ssrLocation == null) {
-            throw new IllegalArgumentException("The ssrLocation must not be null");
-        }
-        StringBuilder hql = new StringBuilder("SELECT o FROM SsrLocation AS o");
-        StringBuilder andClause = new StringBuilder();
-        andClause.append(getAndClauseHql("cdsOrUtr", ssrLocation.getCdsOrUtr()));
-        andClause.append(getAndClauseHql("locationGene", ssrLocation.getLocationGene()));
-        andClause.append(getAndClauseHql("locationInScaffold", ssrLocation.getLocationInScaffold()));
-        andClause.append(getAndClauseHql("pacId", ssrLocation.getPacId()));
-        andClause.append(getAndClauseHql("scaffold", ssrLocation.getScaffold()));
-        andClause.append(getAndClauseHql("seqOfScaffold", ssrLocation.getSeqOfScaffold()));
-        andClause.append(getAndClauseHql("ssr", ssrLocation.getSsr()));
-        andClause.append(getAndClauseHql("strand", ssrLocation.getStrand()));
-        andClause.append(getAndClauseHql("uniGeneId", ssrLocation.getUniGeneId()));
-        if(andClause.length() > 0) {
-            int andIndex = andClause.indexOf("and");
-            andClause.replace(andIndex, andIndex + 3, "where");
-            hql.append(andClause.toString());
-        }
-        EntityManager em = SsrLocation.entityManager();
-        TypedQuery<SsrLocation> q = em.createQuery(hql.toString(), SsrLocation.class);
-        return q;
-    }
-
-    private static String getAndClauseHql(String fieldName, String filedValue) {
-        if(filedValue == null || filedValue.trim().equals("")) {
-            return "";
-        } else {
-            return " and " + fieldName + " = '" + filedValue + "'";
-        }
-    }
 }
